8c57d616dfcbbba8276ec8714149acc6c3592978
zhangyuyangtoushui <127684453+zhangyuyangtoushui@users.noreply.github.com>
2024-05-03 20:21:09 +0800

    Update javdbvip.js

--- javdbvip.js
+++ javdbvip.js
@@ -49,7 +49,7 @@ if (body) {
           }
         } else if (url.includes("/api/v4/movies/")) {
           if (obj?.data?.show_vip_banner) {
-            //obj.data.show_vip_banner = false;
+            obj.data.show_vip_banner = true;
           }
         }
         body = JSON.stringify(obj);
