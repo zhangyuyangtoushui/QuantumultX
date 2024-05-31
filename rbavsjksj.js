42c2f3898ee532d1a5ccf1717ea7ac5a8072cf0a
zhangyuyangtoushui <127684453+zhangyuyangtoushui@users.noreply.github.com>
2024-05-31 18:48:30 +0800

    Update rbavsjksj.js

--- rbavsjksj.js
+++ rbavsjksj.js
@@ -3,7 +3,7 @@ let header = $request.headers;
 
 if (url.includes("/api/v1/movies/") && url.includes("/play?")) {
   header.authorization =
-    "BearereyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTQ0MjQ4MywidXNlcm5hbWUiOiJ5cWMwMDcifQ.l0adZ5o4jao5s9pio7IZpbbdg0HsTdsonzj-cHXh8yw";
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTQ0MjQ4MywidXNlcm5hbWUiOiJ5cWMwMDcifQ.l0adZ5o4jao5s9pio7IZpbbdg0HsTdsonzj-cHXh8yw";
   $done({ headers: header });
 } else {
   $done({});
