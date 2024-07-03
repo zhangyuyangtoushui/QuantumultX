69556caaf78ed0caf980ea05d3ce913f64c246ac
zhangyuyangtoushui <127684453+zhangyuyangtoushui@users.noreply.github.com>
2024-07-03 21:11:19 +0800

    Update rbavsjk.js

--- rbavsjk.js
+++ rbavsjk.js
@@ -10,58 +10,52 @@ hostname = api.pxxgg.xyz, api.ujvnmkx.cn, api.yijingluowangluo.xyz, api.hechuang
 
 */
 
-if (!$response.body) $done({});
-const url = $request.url;
-let body = $response.body;
-
-if (body) {
-  switch (true) {
-    // JavDB
-    case /^https:\/\/(api\.hechuangxinxi\.xyz|jdforrepam\.com)\/api\/v\d\/\w+/.test(url):
-      try {
-        let obj = JSON.parse(body);
-        if (url.includes("/api/v1/ads")) {
-          if (obj?.data?.enabled) {
-            obj.data.enabled = false;
-          }
-          if (obj?.data?.ads) {
-            obj.data.ads = {};
-          }
-        } else if (url.includes("/api/v1/startup")) {
-          if (obj?.data?.splash_ad) {
-            obj.data.splash_ad.enabled = false;
-            obj.data.splash_ad.overtime = 0;
-          }
-          if (obj?.data?.feedback) {
-            obj.data.feedback = {};
-          }
-          if (obj?.data?.settings?.NOTICE) {
-            delete obj.data.settings.NOTICE;
-          }
-          if (obj?.data?.user) {
-  header.authorization =
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MzU4NDg3NywidXNlcm5hbWUiOiJjaHhtMTAyNCJ9.pQib789RWlw2N2hrFLVXF-mj125tpw1HXd_t0HbugZc";
-  $done({ headers: header });
-}
-        } else if (url.includes("/api/v1/users")) {
-          if (obj?.data?.user) {
-  header.authorization =
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MzU4NDg3NywidXNlcm5hbWUiOiJjaHhtMTAyNCJ9.pQib789RWlw2N2hrFLVXF-mj125tpw1HXd_t0HbugZc";
-  $done({ headers: header });
-}
-        } else if (url.includes("/api/v4/movies/")) {
-  header.authorization =
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MzU4NDg3NywidXNlcm5hbWUiOiJjaHhtMTAyNCJ9.pQib789RWlw2N2hrFLVXF-mj125tpw1HXd_t0HbugZc";
-  $done({ headers: header });
-}
-        body = JSON.stringify(obj);
-      } catch (error) {
-        console.log(`JavDB, 出现异常: ` + error);
+// JavDB
+  case /^https:\/\/api\.hechuangxinxi\.xyz\/api\/v\d\/\w+/.test(url):
+    try {
+      let obj = JSON.parse(body);
+      if (url.includes("/api/v1/ads")) {
+        // 首页banner
+        if (obj?.data?.ads?.index_top?.length > 0) {
+          // 黑名单 移除http外链
+          obj.data.ads.index_top = obj.data.ads.index_top.filter((i) => !/https?:\/\//.test(i?.url));
+        }
+        if (obj?.data?.ads?.web_magnets_top?.length > 0) {
+          // 黑名单 移除http外链
+          obj.data.ads.web_magnets_top = obj.data.ads.web_magnets_top.filter((i) => !/https?:\/\//.test(i?.url));
+        }
+      } else if (url.includes("/api/v1/startup")) {
+        // 开屏广告
+        if (obj?.data?.splash_ad) {
+          obj.data.splash_ad.enabled = false;
+          obj.data.splash_ad.overtime = 0;
+        }
+        if (obj?.data?.feedback) {
+          obj.data.feedback = {};
+        }
+        if (obj?.data?.settings?.NOTICE) {
+          delete obj.data.settings.NOTICE;
+        }
+        if (obj?.data?.user) {
+          obj.data.user.vip_expired_at = "2090-12-31T23:59:59.000+08:00";
+          obj.data.user.is_vip = true;
+        }
+      } else if (url.includes("/api/v1/users")) {
+        // 伪装会员
+        if (obj?.data?.user) {
+          obj.data.user.vip_expired_at = "2090-12-31T23:59:59.000+08:00";
+          obj.data.user.is_vip = true;
+        }
+      } else if (url.includes("/api/v4/movies/")) {
+        // 详情页banner
+        if (obj?.data?.show_vip_banner) {
+          obj.data.show_vip_banner = false;
+        }
+      } else {
+        $done({});
       }
-      break;
-    default:
-      break;
-  }
-  $done({ body });
-}
-
+      body = JSON.stringify(obj);
+    } catch (err) {
+      console.log(`JavDB, 出现异常: ` + err);
+    }
+    break;
